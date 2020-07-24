6e250f7bb2d352722c8c282b3d788df6add52237
Abhilash Saj <abhilashsaj@gmail.com>
2020-07-24 20:00:49 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/dashboard/Dashboard'
 import NoteDetails from './components/notes/NoteDetails'
 import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
+import CreateNote from './components/notes/CreateNote'
 class App extends Component {
   render() {
     return (
@@ -16,6 +17,7 @@ class App extends Component {
             <Route path='/note/:id' component={NoteDetails} />
             <Route path='/signin' component={SignIn} />
             <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateNote} />
           </Switch>
         </div>
       </BrowserRouter>
