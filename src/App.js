ae6b0beccf31b155d71c1440ba0b35cbdd33275b
Abhilash Saj <abhilashsaj@gmail.com>
2020-07-24 19:23:38 +0530

    signin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
-
+import NoteDetails from './components/notes/NoteDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 class App extends Component {
   render() {
     return (
@@ -10,7 +12,10 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path='/'component={Dashboard} />
+            <Route exact path='/'component={Dashboard} />
+            <Route path='/note/:id' component={NoteDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
---
+++ src/components/notes/NoteSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NoteSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title ">Note title</span>
+        <p>Hope you have a good day</p>
+        <p className="grey-text">24th July, 2am</p>
+      </div>
+    </div>
+  )
+}
+
+export default NoteSummary
